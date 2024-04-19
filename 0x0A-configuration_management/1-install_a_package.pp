50d881ba7e3195cce193fb7e19592316178a7313
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-04-19 21:08:22 +0300

    Install another package 'werkzeug 2.2.2' to prevent flask error when importing newer version

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,13 @@
 #!/usr/bin/pup
 # Puppet file to install flask package
 
+package {'werkzeug':
+  ensure   => '2.2.2',
+  provider => 'pip3',
+}
+
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['werkzeug'],
 }
