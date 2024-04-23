a64a4dba0754917a57140ac461e6f8e57410b721
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-04-23 19:04:12 +0000

    Add a path attribute in the file_line resource

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -7,6 +7,7 @@ file { 'index.nginx-debian.html':
   ensure  => file,
   path    => '/var/www/html/index.nginx-debian.html',
   content => 'Hello World!',
+  require => Package['nginx'],
 }
 
 $newline = 'rewrite /redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
@@ -15,4 +16,10 @@ file_line { 'redirect':
   line    => "server_name _;\n\t${newline}",
   match   => 'server_name _;',
   replace => true,
+  path    => '/etc/nginx/sites-available/default',
+}
+
+service { 'nginx':
+  ensure => running,
+  require => Package['nginx'],
 }
