0e0b7411a3f9074f42c743dd401b2c617ee29c28
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-04-29 22:08:01 +0000

    Add initial configuration and make sure to restart nginx

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,36 @@
 # Puppet file to configure nginx servers to add custom headers to their response
 
-exec { 'custom_header':
-  command => 'sudo sed -i "s/location \/ {/location \/ {\n\t\tadd_header X-Served-By $(hostname);/" /etc/nginx/sites-available/default',
-    path  => '/usr/bin'
+package { 'nginx':
+  ensure => installed,
+  name   => 'nginx',
+}
+
+file { 'index.nginx-debian.html':
+  ensure  => file,
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+$newline = "rewrite /redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;"
+
+file_line { 'redirect':
+  line    => "server_name _;\n\t${newline}",
+  match   => "server_name _;",
+  path    => '/etc/nginx/sites-available/default',
+  require => Package['nginx'],
+}
+
+file_line { 'custom_header':
+  ensure  => present,
+  line    => "\t\tadd_header X-Served-By ${hostname}",
+  after   => '\n\tlocation / {',
+  path    => '/etc/nginx/sites-available/default',
+  require => Package['nginx'],
+}
+
+exec { 'restart_nginx':
+  command => 'sudo service nginx restart',
+  path    => '/usr/bin',
+  require => Package['nginx'],
 }
