298e7f07202437d651f960018665cda7fd78d3a7
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-04-29 21:51:06 +0000

    Rewrite manifest with the exec resource instead of file_line

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,6 @@
 # Puppet file to configure nginx servers to add custom headers to their response
 
-file_line { 'custom_header':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  match  => 'add_header X-Served-By ',
-  line   => "\t\tadd_header X-Served-By ${hostname}",
-  after  => 'location / {',
+exec { 'custom_header':
+  command => 'sudo sed -i "s/location \/ {/location \/ {\n\t\tadd_header X-Served-By $(hostname);/" /etc/nginx/sites-available/default',
+    path  => '/usr/bin'
 }
