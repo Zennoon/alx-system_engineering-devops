f87b29ce9af7566cede5fcb7a4b2b6d559acc886
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-05-20 20:17:47 +0000

    Write a python script that retrieves JSON data from an API and stores it in JSON format in a file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+
+Contains:
+    Misc
+    ====
+    Script to fetch data from the jsonplaceholder.typicode api
+    about users' TODOs and store relevant information in csv
+    format to a file
+
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    ARGV = sys.argv
+    user_id = ARGV[1]
+    API_URL = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    employee_res = requests.get(API_URL).json()
+    employee_username = employee_res.get("username")
+
+    tasks_res = requests.get(API_URL + "/todos").json()
+
+    with open("{}.json".format(ARGV[1]), 'w', encoding="utf-8") as f:
+        output = {"2": [{
+            "task": task.get("title"),
+            "completed": task.get("completed"),
+            "username": employee_username,
+        } for task in tasks_res]}
+        json.dump(output, f)
