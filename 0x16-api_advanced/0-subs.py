c95b39ba62393024e2845cd6f33d0c6c5e81dde2
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-06-04 09:44:39 +0000

    Modify script

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,36 @@
 #!/usr/bin/python3
 """
-number of subscribers for a given subreddit
+Contains:
+    Functions
+    =========
+    number_of_subscribers(subreddit) - Communicates with the Reddit API to get
+    the number of subscribers for a given subreddit
 """
-
 from requests import get
 
 
 def number_of_subscribers(subreddit):
     """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
+    Communicates with the Reddit API to get the number of subscribers
+    for a given subreddit.
 
+    Args:
+        subreddit (str): The name of the subreddit whose subscribers count
+        is extracted
+
+    Returns:
+        int: The number of subscribers of the given subreddit (0 if the
+        subreddit doesn't exist)
+    """
     if subreddit is None or not isinstance(subreddit, str):
         return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
-    try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
-        return 0
+    num_subscribers = 0
+    endpoint_url = "https://www.reddit.com/r/{}/about.json"
+    res = get(endpoint_url.format(subreddit),
+              headers={
+                  "User-agent": "Google Chrome Version 81.0.4044.129"
+              })
+    data = res.json()
+    if data.get("data") is not None:
+        num_subscribers = data["data"]["subscribers"]
+    return (num_subscribers)
