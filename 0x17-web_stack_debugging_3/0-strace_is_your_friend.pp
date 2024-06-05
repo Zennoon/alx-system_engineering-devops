9867a8eb981ff1403cf82c36a46aa0a61e657d17
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-06-05 14:49:24 +0000

    Fix issues with puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,8 +3,8 @@
 $fpath = '/var/www/html/wp-settings.php'
 exec { 'phpp_to_php':
   command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' ${fpath}",
-  path     => '/bin'
+  path    => '/bin'
 } -> service { 'restart_apache2':
-  name   => 'apache2',
-  ensure => 'running'
+  ensure => 'running',
+  name   => 'apache2'
 }
